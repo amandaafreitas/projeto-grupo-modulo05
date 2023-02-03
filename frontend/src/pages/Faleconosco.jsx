771f60d7edc9d3b2a1bb6954ac5e73bfbc04c386
Caio0G <113181402+Caio0G@users.noreply.github.com>
2023-02-03 11:42:32 -0300

    Atualização do código

--- frontend/src/pages/Faleconosco.jsx
+++ frontend/src/pages/Faleconosco.jsx
@@ -1,29 +1,34 @@
-import "../styles/faleconosco.css"
+import "../styles/faleconosco.css";
 
 const Faleconosco = () => {
-    return (
-        <>
-        <h1>Entre em contato conosco :
-        </h1>
-        <div>
-         <div>
-            <h1>Qual o motivo ?</h1>
-            <form action="" id="forma">
-            <label htmlFor="subject">Assunto</label>
-            <select name="subject" id="">
-             <option value="dúvida">dúvida</option>
-             <option value="reclamação">reclamação</option>
-             <option value="sugestão">sugestão</option>  
-            </select>
-            <input type="textarea" />
-            <input type="button" value="Enviar" />
-            </form>
-
-         </div> 
-        </div>
-        </>
-
-    );
+  return (
+    <>
+      <div>
+        <h1>Entre em contato conosco :</h1>
+      </div>
+      <div>
+        <form action="" id="input">
+          Nome: <input type="text" /> <br></br>
+          Usuário: <input type="text" />
+        </form>
+      </div>
+      <div>
+        <h1>Qual o motivo ?</h1>
+      </div>
+      <div>
+        <form action="" id="forma">
+          <label htmlFor="subject">Assunto</label>
+          <select name="subject" id="">
+            <option value="dúvida">dúvida</option>
+            <option value="reclamação">reclamação</option>
+            <option value="sugestão">sugestão</option>
+          </select>
+          <input type="textarea" />
+          <input type="button" value="Enviar" />
+        </form>
+      </div>
+    </>
+  );
 };
 
-export default Faleconosco
+export default Faleconosco;
