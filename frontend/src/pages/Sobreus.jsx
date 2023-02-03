35e5096772a2ca6fd056698ac2da05baa45504bb
Caio0G <113181402+Caio0G@users.noreply.github.com>
2023-02-02 22:52:21 -0300

    Adição da página CSS

--- frontend/src/pages/Sobreus.jsx
+++ frontend/src/pages/Sobreus.jsx
@@ -1,8 +1,9 @@
+import "../styles/sobreus.css"
 
 const Sobreus = () => {
     return (
         <>
-        <h1>Teste três</h1>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptates esse et placeat sint laborum eligendi cupiditate, voluptate cum ducimus voluptatibus quo consequatur assumenda vitae pariatur? Consequuntur similique fuga fugiat.</h1>
         </>
     );
 };
